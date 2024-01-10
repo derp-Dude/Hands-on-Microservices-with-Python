ff202de9ed4de19624893af7bb31b3d83c05504d
root <root@ip-172-31-41-137.ec2.internal>
2024-01-10 13:54:56 +0000

    dependency issue fix

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Flask
+from flask import Flask
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from frontend import frontend_blueprint
@@ -15,10 +15,9 @@ bootstrap = Bootstrap(app)
 app.config.update(dict(
     SECRET_KEY="powerful secretkey",
     WTF_CSRF_SECRET_KEY="a csrf secret key",
-    PRODUCT_SERVICE= 'http://192.168.99.100:8081'
+    PRODUCT_SERVICE='http://0.0.0.0:8081'
 ))
 
 app.register_blueprint(frontend_blueprint)
 
 app.run(debug=True, host='0.0.0.0')
-
